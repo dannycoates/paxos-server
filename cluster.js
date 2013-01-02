d7c413dd579cfdbf2032df86bbb1d9e70beddccc
Danny Coates <dannycoates@gmail.com>
2013-01-01 21:45:54 -0800

    first example http server

---
+++ cluster.js
@@ -0,0 +1,19 @@
+var spawn = require('child_process').spawn
+var argv = require('optimist').argv
+
+var n = argv.n || 3
+var processes = []
+var endpoints = []
+
+for (var i = 0; i < n; i++) {
+	endpoints.push('localhost:' + (11111 + i))
+}
+
+for (var i = 0; i < n; i++) {
+	var args = ['./http-server.js']
+	args.push('--id=' + (i + 1))
+	args.push('--listen=' + (11111 + i))
+	var proc = spawn('node', args.concat(endpoints), { cwd: __dirname })
+	proc.stdout.pipe(process.stdout)
+	processes.push(proc)
+}
