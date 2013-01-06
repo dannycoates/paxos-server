6efe94d3f69a693a11d26fecae503e7138ca050e
Danny Coates <dannycoates@gmail.com>
2013-01-06 14:16:29 -0800

    added 'learn' endpoint and debug logging

--- basic-server.js
+++ basic-server.js
@@ -8,6 +8,7 @@ var peers = argv._
 var port = argv.listen
 var majority = Math.floor(peers.length / 2) + 1
 var paxos = new Paxos(argv.id, majority)
+console.log('starting', paxos.proposer.id, process.pid)
 
 for (var i = 0; i < peers.length; i++) {
 	var peer = peers[i]
@@ -33,7 +34,7 @@ function broadcast(name, message) {
 }
 
 function reply(name, message) {
-	var peer = cluster[message.proposer]
+	var peer = cluster[message.requester]
 	request.post(
 		{
 			uri: 'http://' + peer + '/paxos/' + name,
@@ -45,12 +46,14 @@ function reply(name, message) {
 
 function handle(handler) {
 	return function (req, res) {
-		handler(Paxos.Proposal.parse(req.body))
+		var proposal = Paxos.Proposal.parse(req.body)
+		console.log('id %s %s\t%s', argv.id, req.path.replace('/paxos/',''), proposal)
+		handler(proposal)
 		res.send(202)
 	}
 }
 
-var api = ['prepare', 'promised', 'accept', 'accepted', 'rejected']
+var api = ['prepare', 'promised', 'accept', 'accepted', 'rejected', 'learn']
 
 for (var i = 0; i < api.length; i++) {
 	var call = api[i]
@@ -62,11 +65,12 @@ paxos.on('promised', reply.bind(null, 'promised'))
 paxos.on('accept', broadcast.bind(null, 'accept'))
 paxos.on('accepted', reply.bind(null, 'accepted'))
 paxos.on('rejected', reply.bind(null, 'rejected'))
+paxos.on('data', broadcast.bind(null, 'learn'))
 
 app.post('/submit', function (req, res) {
 	var x = Object.keys(req.body)[0]
-	paxos.submit(x)
 	console.log('submit', x)
+	paxos.submit(x)
 	paxos.once('learned', function (fact) {
 		res.send(fact)
 	})
